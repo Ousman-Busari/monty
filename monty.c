8454f1b913afb0860ff025e8d88537a6fa5d1c74
Ousman-Busari <usman.traid@gmail.com>
2022-10-08 06:55:43 -0400

    monty main edited

--- monty.c
+++ monty.c
@@ -6,10 +6,10 @@ int main(int ac, char **av)
 	int exit_code = EXIT_SUCCESS;
 
 	if (ac != 2)
-		return (98); /* (usage_error()); */
+		return (usage_error());
 	stream_fd = fopen(av[1], "r");
 	if (stream_fd == NULL)
-		return (98); /* (f_open_error(av[1])); */
+		return (file_error(av[1]));
 	exit_code = monty_run(stream_fd);
 	fclose(stream_fd);
 	return (exit_code);
